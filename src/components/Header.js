390bed8602a4d886dee43281ae04a5106482d62a
Cas <cas@interglobal.vision>
2018-08-22 16:38:21 -0500

    Add <Header/>

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    marginBottom: 8,
+  },
+  toolbar: {
+  }
+};
+
+function Header(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="title" color="inherit">
+            Pokedex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Header);
